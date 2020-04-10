c9b00a0a9558ba94e09ba8dc86465c17f6541691
LuanadeSouza <you@example.com>
2020-04-10 11:28:12 -0300

    " construindo implementacao logica Pagamento"

--- app/src/main/java/com/example/agenciaapplication/ui/activity/PagamentoActivity.java
+++ app/src/main/java/com/example/agenciaapplication/ui/activity/PagamentoActivity.java
@@ -1,10 +1,15 @@
 package com.example.agenciaapplication.ui.activity;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.example.agenciaapplication.R;
+import com.example.agenciaapplication.model.Pacote;
+import com.example.agenciaapplication.util.MoedaUtil;
+
+import java.math.BigDecimal;
 
 public class PagamentoActivity extends AppCompatActivity {
 
@@ -16,6 +21,14 @@ public class PagamentoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pagamento);
         setTitle(TITLE_PAGAMENTO);
 
+        Pacote pacoteSaoPaulo = new Pacote("São Paulo", "sao_paulo_sp", 2, new BigDecimal("243.99"));
 
+        mostraPreco(pacoteSaoPaulo);
+    }
+
+    private void mostraPreco(Pacote pacote) {
+        TextView preco = findViewById(R.id.pagamento_preco_pacote);
+        String moedaBrasileira = MoedaUtil.getformat(pacote.getPreco());
+        preco.setText(moedaBrasileira);
     }
 }
