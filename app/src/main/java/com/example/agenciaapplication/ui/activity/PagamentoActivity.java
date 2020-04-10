aa3a40d4aaf7d21ab58a16a0322fbc88d5592022
LuanadeSouza <you@example.com>
2020-04-10 15:21:04 -0300

    " construindo a implementacao logica tela de confirmacao"

--- app/src/main/java/com/example/agenciaapplication/ui/activity/PagamentoActivity.java
+++ app/src/main/java/com/example/agenciaapplication/ui/activity/PagamentoActivity.java
@@ -1,5 +1,6 @@
 package com.example.agenciaapplication.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -24,6 +25,9 @@ public class PagamentoActivity extends AppCompatActivity {
         Pacote pacoteSaoPaulo = new Pacote("São Paulo", "sao_paulo_sp", 2, new BigDecimal("243.99"));
 
         mostraPreco(pacoteSaoPaulo);
+
+        Intent intent = new Intent(this, ResumoCompraActivity.class);
+        startActivity(intent);
     }
 
     private void mostraPreco(Pacote pacote) {
--- app/src/main/java/com/example/agenciaapplication/ui/activity/ResumoCompraActivity.java
+++ app/src/main/java/com/example/agenciaapplication/ui/activity/ResumoCompraActivity.java
@@ -2,17 +2,60 @@ package com.example.agenciaapplication.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.agenciaapplication.R;
+import com.example.agenciaapplication.model.Pacote;
+import com.example.agenciaapplication.util.DataUtil;
+import com.example.agenciaapplication.util.MoedaUtil;
+import com.example.agenciaapplication.util.ResourceUtil;
+
+import org.w3c.dom.Text;
+
+import java.math.BigDecimal;
 
 public class ResumoCompraActivity extends AppCompatActivity {
+
+    public static final String TITLE_RESUMO = "Resumo da compra";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumo_compra);
 
-        setTitle("Resumo da compra");
+        setTitle(TITLE_RESUMO);
+
+        Pacote pacoteSaoPaulo = new Pacote("São Paulo", "sao_paulo_sp", 2, new BigDecimal("243.99"));
+
+        mostraLocal(pacoteSaoPaulo);
+        mostraImage(pacoteSaoPaulo);
+        mostraDate(pacoteSaoPaulo);
+        mostraPreco(pacoteSaoPaulo);
+    }
+
+    private void mostraPreco(Pacote pacote) {
+        TextView preco = findViewById(R.id.resumo_compra_valor);
+        String moedaBrasileira = MoedaUtil.getformat(pacote.getPreco());
+        preco.setText(moedaBrasileira);
+    }
+
+    private void mostraDate(Pacote pacote) {
+        TextView data = findViewById(R.id.resumo_compra_data);
+        String periodoEmTexto = DataUtil.periodoEmTexto(pacote.getDias());
+        data.setText(periodoEmTexto);
+    }
+
+    private void mostraImage(Pacote pacote) {
+        ImageView image = findViewById(R.id.resumo_compra_image);
+        Drawable drawableDoPacote = ResourceUtil.getDrawable(this, pacote.getImage());
+        image.setImageDrawable(drawableDoPacote);
+    }
+
+    private void mostraLocal(Pacote pacote) {
+        TextView local = findViewById(R.id.resumo_compra_local);
+        local.setText(pacote.getLocal());
     }
 }
